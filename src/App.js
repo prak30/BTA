80294dbbff3885fce252710ee1cb95411854b65f
pranav <katkarpranav4@gmail.com>
2024-06-08 09:59:23 +0530

    A dded functionality to connect to wallet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Web3Modal from "web3modal";
+import "./App.css";
+import { useState } from "react";
+
+const ethers = require("ethers");
+
+const providerOptions = {};
 
 function App() {
+  const [web3Provider, setWeb3Provider] = useState(null);
+  async function connectWallet() {
+    try {
+      const web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions,
+      });
+      const web3ModalInstance = await web3Modal.connect();
+      const web3ModalProvider = new ethers.providers.Web3Provider(
+        web3ModalInstance
+      );
+      console.log(web3ModalProvider);
+      if (web3ModalProvider) {
+        setWeb3Provider(web3ModalProvider);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Web3Modal connection!!!</h1>
+        {web3Provider == null ? (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        ) : (
+          <div>
+            <p>Connected</p>
+            <p>Address : {web3Provider.provider.selectedAddress}</p>
+          </div>
+        )}
       </header>
     </div>
   );
